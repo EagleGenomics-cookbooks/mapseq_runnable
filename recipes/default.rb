45b816a63699b3de6c53b2e5427632fee38c4dd7
maurosaporita <mauro.saporita@eaglegenomics.com>
2018-10-17 15:15:27 +0100

    Fixed data bags and role

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,9 +5,9 @@
 # Copyright:: 2018, Eagle Genomics Ltd, All Rights Reserved.
 
 Runnable_setup 'mapseq' do
-  #tool_cookbook_name    'mapseq'
-  #runnable_git_repo     'mapseq_runnable'
-  #runnable_git_revision 'master'
-  # build_ehive false
+  tool_cookbook_name    'mapseq'
+  runnable_git_repo     'mapseq_runnable'
+  runnable_git_revision 'master'
+  build_ehive false
   :create
 end
