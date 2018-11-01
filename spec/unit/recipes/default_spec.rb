b01e3d0a084797449e170d53039d3f0f75951108
maurosaporita <mauro.saporita@eaglegenomics.com>
2018-11-01 10:10:30 +0000

    commented spec test

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -4,32 +4,32 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
-require 'spec_helper'
+#require 'spec_helper'
 
-describe 'mapseq_runnable::default' do
-  context 'When all attributes are default, on Ubuntu 16.04' do
-    let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
-      runner.converge(described_recipe)
-    end
+#describe 'mapseq_runnable::default' do
+#  context 'When all attributes are default, on Ubuntu 16.04' do
+#    let(:chef_run) do
+#      # for a complete list of available platforms and versions see:
+#      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
+#      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+#      runner.converge(described_recipe)
+#    end
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
-    end
-  end
+#    it 'converges successfully' do
+#      expect { chef_run }.to_not raise_error
+#    end
+#  end
 
-  context 'When all attributes are default, on CentOS 7.4.1708' do
-    let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.4.1708')
-      runner.converge(described_recipe)
-    end
+#  context 'When all attributes are default, on CentOS 7.4.1708' do
+#    let(:chef_run) do
+#      # for a complete list of available platforms and versions see:
+#      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
+#      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.4.1708')
+#      runner.converge(described_recipe)
+#    end
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
-    end
-  end
-end
+#    it 'converges successfully' do
+#      expect { chef_run }.to_not raise_error
+#    end
+#  end
+#end
